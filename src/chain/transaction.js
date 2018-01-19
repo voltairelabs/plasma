cba1d70a12fe69b399b9c6271a1bbfa98187384c
Jaynti Kanani <jdkanani@gmail.com>
2018-01-20 02:07:45 +0530

    add deposit testcase

---
+++ src/chain/transaction.js
@@ -0,0 +1,109 @@
+import utils from 'ethereumjs-util'
+import {Buffer} from 'safe-buffer'
+
+const BN = utils.BN
+const rlp = utils.rlp
+const getFields = () => [
+  {
+    name: 'blknum1',
+    default: new Buffer([])
+  },
+  {
+    name: 'txindex1',
+    default: new Buffer([])
+  },
+  {
+    name: 'oindex1',
+    default: new Buffer([])
+  },
+  {
+    name: 'blknum2',
+    default: new Buffer([])
+  },
+  {
+    name: 'txindex2',
+    default: new Buffer([])
+  },
+  {
+    name: 'oindex2',
+    default: new Buffer([])
+  },
+  {
+    name: 'newowner1',
+    length: 20,
+    default: utils.zeros(20)
+  },
+  {
+    name: 'amount1',
+    default: new Buffer([])
+  },
+  {
+    name: 'newowner2',
+    length: 20,
+    default: utils.zeros(20)
+  },
+  {
+    name: 'amount2',
+    default: new Buffer([])
+  },
+  {
+    name: 'fee',
+    default: new Buffer([])
+  },
+  {
+    name: 'sig1',
+    length: 65,
+    default: utils.zeros(65)
+  },
+  {
+    name: 'sig2',
+    length: 65,
+    default: utils.zeros(65)
+  }
+]
+
+export default class Transaction {
+  constructor(data) {
+    utils.defineProperties(this, getFields(), data)
+  }
+
+  hash(includeSignature = false) {
+    let items
+    if (includeSignature) {
+      items = this.raw
+    } else {
+      items = this.raw.slice(0, this.raw.length - 2)
+    }
+
+    // create hash
+    return utils.rlphash(items)
+  }
+
+  /**
+   * sign a transaction with a given a private key
+   * @param {Buffer} privateKey
+   */
+  sign1(privateKey) {
+    this.sign1 = utils.ecsign(this.hash(false), privateKey)
+    return this.sign1
+  }
+
+  /**
+   * sign a transaction with a given a private key
+   * @param {Buffer} privateKey
+   */
+  sign2(privateKey) {
+    this.sign2 = utils.ecsign(this.hash(false), privateKey)
+    return this.sign2
+  }
+
+  serializeTx(includeSignature = false) {
+    if (includeSignature) {
+      return this.serialize()
+    }
+
+    const items = this.raw.slice(0, this.raw.length - 2)
+    // create hash
+    return rlp.encode(items)
+  }
+}
--- test/RootChain.js
+++ test/RootChain.js
@@ -1,5 +1,59 @@
+import utils from 'ethereumjs-util'
+
 import assertRevert from './helpers/assertRevert'
 
+// import chain components
+import Transaction from '../src/chain/transaction'
+
+// require root chain
+let RootChain = artifacts.require('./RootChain.sol')
+
+const BN = utils.BN
+const keyPair = require('./keyPair')
+
 contract('Root chain', function(accounts) {
-  describe('deposit', async function() {})
+  describe('deposit', async function() {
+    let rootChain
+    let owner
+
+    // before task
+    before(async function() {
+      rootChain = await RootChain.new({from: accounts[0]})
+      owner = keyPair.address1 // same as accounts[0]
+    })
+
+    it('should allow user to deposit ETH into plasma chain', async function() {
+      const value = new BN(web3.toWei(1, 'ether'))
+      const tx = new Transaction([
+        new Buffer([]), // block number 1
+        new Buffer([]), // tx number 1
+        new Buffer([]), // previous output number 1 (input 1)
+        new Buffer([]), // block number 2
+        new Buffer([]), // tx number 2
+        new Buffer([]), // previous output number 2 (input 2)
+
+        Buffer.from(utils.stripHexPrefix(owner), 'hex'), // output address 1
+        value.toArrayLike(Buffer, 'be', 32), // value for output 2
+
+        utils.zeros(20), // output address 2
+        new Buffer([]), // value for output 2
+
+        new Buffer([]) // fee
+      ])
+
+      // serialize tx bytes
+      const txBytes = utils.addHexPrefix(tx.serializeTx().toString('hex'))
+
+      // deposit
+      const receipt = await rootChain.deposit(txBytes, {
+        from: owner,
+        value: value
+      })
+
+      assert.equal(receipt.logs.length, 1)
+      assert.equal(receipt.logs[0].event, 'Deposit')
+      assert.equal(receipt.logs[0].args.depositor, owner)
+      assert.equal(receipt.logs[0].args.amount.toString(), value.toString())
+    })
+  })
 })
