036330dd99d4b1f7066bac82276e6281f86a67cc
Jaynti Kanani <jdkanani@gmail.com>
2018-01-23 14:20:13 +0530

    add transfer (with one input) test case

--- test/RootChain.js
+++ test/RootChain.js
@@ -3,6 +3,7 @@
 import utils from 'ethereumjs-util'
 
 import assertRevert from './helpers/assertRevert'
+import {mineToBlockHeight} from './helpers/utils'
 
 // import chain components
 import Transaction from '../src/chain/transaction'
@@ -17,7 +18,7 @@ const keyPair = require('./keypair')
 keyPair.key1 = utils.toBuffer(keyPair.key1)
 
 contract('Root chain', function(accounts) {
-  describe('deposit and withdraw', async function() {
+  describe('deposit and start withdraw:', async function() {
     const value = new BN(web3.toWei(1, 'ether'))
 
     let rootChain
@@ -67,9 +68,7 @@ contract('Root chain', function(accounts) {
       const value = new BN(web3.toWei(1, 'ether'))
 
       // serialize tx bytes
-      const txBytes = utils.addHexPrefix(
-        depositTx.serializeTx().toString('hex')
-      )
+      const txBytes = utils.bufferToHex(depositTx.serializeTx())
 
       // deposit
       let [childChainRoot, t] = await rootChain.getChildChain(1)
@@ -80,7 +79,9 @@ contract('Root chain', function(accounts) {
       // depositTx.sign1(keyPair.key1) // sign1
       // depositTx.sign2(keyPair.key2) // sign2
       const tree = new FixedMerkleTree(16, [merkleHash])
-      const proof = utils.bufferToHex(Buffer.concat(tree.getPlasmaProof(0)))
+      const proof = utils.bufferToHex(
+        Buffer.concat(tree.getPlasmaProof(merkleHash))
+      )
       const sigs = utils.bufferToHex(
         Buffer.concat([
           depositTx.sig1,
@@ -104,6 +105,70 @@ contract('Root chain', function(accounts) {
       assert.equal(user, owner)
       assert.equal(amount.toString(), value)
       assert.deepEqual([1, 0, 0], pos.map(p => p.toNumber()))
+
+      // trying again shoul fail
+      await assertRevert(
+        rootChain.startExit(
+          [1, 0, 0], // [blockNumber, txNumber, oIndex]
+          txBytes,
+          proof,
+          sigs,
+          {
+            from: owner
+          }
+        )
+      )
+    })
+
+    it('should allow user to create new UTXO from UTXO', async function() {
+      let transferTx = new Transaction([
+        utils.toBuffer(1), // block number for first input
+        new Buffer([]), // tx number for 1st input
+        new Buffer([]), // previous output number 1 (as 1st input)
+        new Buffer([]), // block number 2
+        new Buffer([]), // tx number 2
+        new Buffer([]), // previous output number 2 (as 2nd input)
+
+        utils.toBuffer(owner), // output address 1
+        value.toArrayLike(Buffer, 'be', 32), // value for output 2
+
+        utils.zeros(20), // output address 2
+        new Buffer([]), // value for output 2
+
+        new Buffer([]) // fee
+      ])
+
+      // serialize tx bytes
+      const transferTxBytes = utils.bufferToHex(transferTx.serializeTx())
+
+      // generate proof
+      transferTx.sign1(keyPair.key1) // sign1
+      // transferTx.sign2(keyPair.key2) // sign2
+      const merkleHash = transferTx.merkleHash()
+      const tree = new FixedMerkleTree(16, [merkleHash])
+      const proof = utils.bufferToHex(
+        Buffer.concat(tree.getPlasmaProof(merkleHash))
+      )
+
+      // submit new block - must throw before atleast 6 blocks
+      await assertRevert(
+        rootChain.submitBlock(utils.bufferToHex(tree.getRoot()))
+      )
+
+      // mine 7 more blocks
+      const oldChildBlockNumber = (await rootChain.currentChildBlock()).toNumber()
+      const lastBlock = web3.eth.blockNumber
+      await mineToBlockHeight(web3.eth.blockNumber + 7)
+
+      // try again by submitting block
+      const receipt = await rootChain.submitBlock(
+        utils.bufferToHex(tree.getRoot())
+      )
+
+      assert.equal(
+        (await rootChain.currentChildBlock()).toNumber(),
+        oldChildBlockNumber + 1
+      )
     })
   })
 })
